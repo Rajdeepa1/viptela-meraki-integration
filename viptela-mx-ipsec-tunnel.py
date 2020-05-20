a78a7e4f7f040fc70fb46a352d3b660d865a02f8
suchandanreddy <saisuchandan@gmail.com>
2020-05-20 12:19:47 -0700

    viptela mx script changes

--- viptela-mx-ipsec-tunnel.py
+++ viptela-mx-ipsec-tunnel.py
@@ -58,6 +58,72 @@ def get_logger(logfile, level):
     return None
 
 
+def get_device_ids(jsessionid,token,template_id):
+
+    if token is not None:
+        headers = {'Content-Type': "application/json",'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
+    else:
+        headers = {'Content-Type': "application/json",'Cookie': jsessionid}
+
+    base_url = "https://%s:%s/dataservice"%(vmanage_host,vmanage_port)
+
+    api_url = '/template/device/config/attached/' + template_id
+
+    url = base_url + api_url
+
+    response = requests.get(url=url, headers=headers,verify=False)
+
+    if response.status_code == 200:
+        device_ids = []
+        for device in response.json()['data']:
+            device_ids.append(device['uuid'])
+        if logger is not None:
+            logger.info("Device ids " + str(device_ids))
+        return device_ids
+    else:
+        if logger is not None:
+            logger.error("Failed to get device ids " + str(response.text))
+        exit()
+
+def get_device_inputs(jsessionid,token,template_id, device_ids):
+
+    if token is not None:
+        headers = {'Content-Type': "application/json",'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
+    else:
+        headers = {'Content-Type': "application/json",'Cookie': jsessionid}
+
+    payload = {
+        'templateId': template_id,
+        'deviceIds': device_ids,
+        'isEdited': True,
+        'isMasterEdited': False
+    }
+
+    base_url = "https://%s:%s/dataservice"%(vmanage_host,vmanage_port)
+
+    api_url = '/template/device/config/input'
+
+    url = base_url + api_url    
+
+    response = requests.post(url=url, headers=headers, data=json.dumps(payload), verify=False)
+
+    if response.status_code == 200:
+
+        device_inputs = response.json()['data']
+
+        for input in device_inputs:
+            input['csv-templateId'] = template_id
+    
+        if logger is not None:
+            logger.info("Device config input" + str(device_inputs))
+    else:
+        if logger is not None:
+            logger.error("Failed to get device config input " + str(response.text))
+        exit()
+
+    return device_inputs
+
+
 class Authentication:
 
     @staticmethod
@@ -195,6 +261,7 @@ class create_ipsec_tunnel:
             tunnel_data["template_name"] = "viptela_mx_ipsec_primary"
             tunnel_data["device_type"] = device_info["device_type"]
             tunnel_data["viptela_mx_ipsec_if_name"] = "viptela_mx_ipsec_interface_1"
+            tunnel_data["viptela_mx_ipsec_if_ipv4_address"] = "viptela_mx_ipsec_ipv4_add_1"
             tunnel_data["viptela_mx_ipsec_if_tunnel_source_ip"]   = "viptela_mx_ipsec_src_1"
             tunnel_data["viptela_mx_ipsec_if_tunnel_destination"] = "viptela_mx_ipsec_dst_1"
             tunnel_data["viptela_mx_ipsec_if_pre_shared_secret"] = "viptela_mx_ipsec_psk_1"
@@ -247,10 +314,26 @@ class create_ipsec_tunnel:
 
         feature_template_list = feature_template_ids["generalTemplates"]
 
-        for item in feature_template_list:
+        '''for item in feature_template_list:
             if item["templateType"] == "vpn-vedge":
                 sub_templates = item["subTemplates"]
                 sub_templates.append(ipsec_templateid[0])
+                break'''
+
+        service_vpn_templates = list()
+            
+        for index,item in enumerate(feature_template_list):
+            if item["templateType"] == "vpn-vedge":
+                sub_templates = item["subTemplates"]
+                sub_templates.append(ipsec_templateid[0])
+                #sub_templates.append(ipsec_templateid[1])
+                temp = index+2
+                while(1):
+                    if feature_template_list[temp]['templateType'] == 'vpn-vedge':
+                        service_vpn_templates.append(feature_template_list[temp]['templateId'])
+                    temp = temp+1
+                    if len(feature_template_list) < temp+1:
+                        break
                 break
             
         payload = {
@@ -316,7 +399,9 @@ class create_ipsec_tunnel:
             sys_ip = item1["csv-deviceIP"]
             for item2 in ipsec_parameters:
                 if sys_ip == item2["device_sys_ip"]:
-                    item1["/0/viptela_mx_ipsec_interface_1/interface/if-name"] = "ipsec1"
+                    temp_pri_ipsec_id = item2["pri_ipsec_id"] # to use ipsec interface id in service vpn template  update
+                    item1["/0/viptela_mx_ipsec_interface_1/interface/if-name"] = item2["pri_ipsec_id"]
+                    item1["/0/viptela_mx_ipsec_interface_1/interface/ip/address"] = item2["pri_ipsec_ip"]
                     item1["/0/viptela_mx_ipsec_interface_1/interface/tunnel-source"] = item2["viptela_mx_primary_src_ip"]
                     item1["/0/viptela_mx_ipsec_interface_1/interface/tunnel-destination"] = item2["viptela_mx_primary_dst_ip"]
                     item1["/0/viptela_mx_ipsec_interface_1/interface/ike/authentication-type/pre-shared-key/pre-shared-secret"] = item2["pre_shared_key"]
@@ -370,16 +455,193 @@ class create_ipsec_tunnel:
         while(1):
             template_status_res = requests.get(url,headers=headers,verify=False)
             if template_status_res.status_code == 200:
-                if template_status_res.json()['summary']['status'] == "done":
-                    print("\nTemplate push status is done")
-                    if logger is not None:
-                        logger.info("\nTemplate push status is done")
-                    return
+                template_push_status = template_status_res.json()
+                if template_push_status['summary']['status'] == "done":
+                    if 'Success' in template_push_status['summary']['count']:
+                        print("\nUpdated IPsec templates successfully")
+                        if logger is not None:
+                            logger.info("\nUpdated IPsec templates successfully")
+                    elif 'Failure' in template_push_status['summary']['count']:
+                        print("\nFailed to update IPsec templates")
+                        if logger is not None:
+                            logger.info("\nFailed to update IPsec templates " + str(template_push_status["data"][0]["activity"]))
+                        exit()
+                    break
             else:
                 if logger is not None:
                     logger.error("\nFetching template push status failed " + str(template_status_res.text))                
                 print("\nFetching template push status failed")
                 exit()
+
+        # Update service VPN template with IPsec route
+
+        print("\nService VPN Templates list", service_vpn_templates)
+
+        for item in service_vpn_templates:
+            
+            api = "template/feature/object/%s"%item
+            url = self.base_url + api
+
+            service_vpn_def = requests.get(url,headers=headers,verify=False)
+
+            if service_vpn_def.status_code == 200:
+                template_def = service_vpn_def.json()
+
+                ipsec_route_def = template_def["templateDefinition"]["ip"]["ipsec-route"]
+
+                if not ipsec_route_def:
+                    ipsec_route_def["vipType"] = "constant"
+                    ipsec_route_def["vipValue"] = [
+                                                    {
+                                                        "prefix": {
+                                                        "vipObjectType": "object",
+                                                        "vipType": "constant",
+                                                        "vipValue": device_info["service_vpn_ipsec_route"],
+                                                        "vipVariableName": "vpn_ipsec_route_ipsec_route_prefix"
+                                                        },
+                                                        "vpn": {
+                                                        "vipObjectType": "object",
+                                                        "vipType": "constant",
+                                                        "vipValue": 0
+                                                        },
+                                                        "interface": {
+                                                        "vipObjectType": "list",
+                                                        "vipType": "constant",
+                                                        "vipValue": [
+                                                            temp_pri_ipsec_id
+                                                        ],
+                                                        "vipVariableName": "vpn_ipsec_route_ipsec_route_interface"
+                                                        },
+                                                        "priority-order": [
+                                                        "prefix",
+                                                        "vpn",
+                                                        "interface"
+                                                        ]
+                                                    }
+                                                  ]
+
+                    ipsec_route_def["vipObjectType"] = "tree"
+                    ipsec_route_def["vipPrimaryKey"] = [
+                                                         "prefix"
+                                                       ]
+                
+                else:
+                    temp  =            {
+                                            "prefix": {
+                                            "vipObjectType": "object",
+                                            "vipType": "constant",
+                                            "vipValue": device_info["service_vpn_ipsec_route"],
+                                            "vipVariableName": "vpn_ipsec_route_ipsec_route_prefix"
+                                            },
+                                            "vpn": {
+                                            "vipObjectType": "object",
+                                            "vipType": "constant",
+                                            "vipValue": 0
+                                            },
+                                            "interface": {
+                                            "vipObjectType": "list",
+                                            "vipType": "constant",
+                                            "vipValue": [
+                                                temp_pri_ipsec_id
+                                            ],
+                                            "vipVariableName": "vpn_ipsec_route_ipsec_route_interface"
+                                            },
+                                            "priority-order": [
+                                            "prefix",
+                                            "vpn",
+                                            "interface"
+                                            ]
+                                        }
+                    
+                    ipsec_route_def["vipValue"].append(temp)
+
+
+                template_def["templateDefinition"]["ip"]["ipsec-route"] = ipsec_route_def
+
+            api = "template/feature/%s"%item
+            url = self.base_url + api
+
+            payload = {
+                         "templateName" : template_def["templateName"],
+                         "templateDescription" : template_def["templateDescription"],
+                         "templateType" : template_def["templateType"],
+                         "deviceType" : template_def["deviceType"],
+                         "templateMinVersion" : template_def["templateMinVersion"],
+                         "templateDefinition" : template_def["templateDefinition"],
+                         "factoryDefault" : False
+                      }
+
+            payload = json.dumps(payload)
+
+            if logger is not None:
+                logger.info("\nService VPN template JSON payload " + str(payload))
+
+            update_service_vpn = requests.put(url,headers=headers,data=payload,verify=False)
+
+            if update_service_vpn.status_code == 200:
+                master_templates_affected = update_service_vpn.json()["masterTemplatesAffected"]
+            else:
+                if logger is not None:
+                    logger.error("\nFailed to edit Service VPN template " + str(update_service_vpn.text))
+                exit()
+
+            # Get device uuid and csv variables for each template id which is affected by prefix list edit operation
+
+            inputs = []
+
+            for template_id in master_templates_affected:
+                device_ids = get_device_ids(self.jsessionid,self.token,template_id)
+                device_inputs = get_device_inputs(self.jsessionid,self.token,template_id,device_ids)
+                inputs.append((template_id, device_inputs))
+
+
+            device_template_list = []
+            
+            for (template_id, device_input) in inputs:
+                device_template_list.append({
+                    'templateId': template_id,
+                    'isEdited': True,
+                    'device': device_input
+                })
+
+
+            #api_url for CLI template 'template/device/config/attachcli'
+
+            api_url = 'template/device/config/attachfeature'
+
+            url = self.base_url + api_url
+
+            payload = { 'deviceTemplateList': device_template_list }
+
+            response = requests.post(url=url, headers=headers,  data=json.dumps(payload), verify=False)
+
+            if response.status_code == 200:
+                process_id = response.json()["id"]
+                if logger is not None:
+                    logger.info("Attach template process id " + str(response.text))
+            else:
+                if logger is not None:
+                    logger.error("Template attach process failed " + str(response.text)) 
+                exit()    
+
+            api_url = 'device/action/status/' + process_id  
+
+            url = self.base_url + api_url
+
+            while(1):
+                time.sleep(10)
+                response = requests.get(url=url, headers=headers, verify=False)
+                if response.status_code == 200:
+                    if response.json()['summary']['status'] == "done":
+                        logger.info("\nUpdated Service VPN template %s successfully"%item)
+                        print("\nUpdated Service VPN template %s successfully"%item)
+                        break
+                    else:
+                        continue
+                else:
+                    logger.error("\nFetching template push status failed " + str(response.text))
+                    exit()
+
 
 if __name__ == "__main__":
     try:
@@ -398,6 +660,8 @@ if __name__ == "__main__":
         device_template_name = config["device_template_name"]
         api_key = config["api_key"]
         orgName = config["orgName"]
+        service_vpn_ipsec_route = config.get("service_vpn_ipsec_route","0.0.0.0/0")
+
 
         '''
         Below is a list of all the necessary Meraki credentials
@@ -526,6 +790,10 @@ if __name__ == "__main__":
         # Loop over edge routers to create and deploy ipsec tunnel to viptela_mx vpn endpoint
         for device in config["vip_devices"]:
             print("Device: {}".format(device["system_ip"]))
+
+            pri_ipsec_id = device.get("pri_ipsec_id","ipsec254")
+            pri_ipsec_ip = device.get("pri_ipsec_ip","10.10.10.1/30")
+
             source_ip = ipsec_tunnel.get_interface_ip(device["system_ip"],device["vpn0_source_interface"])
 
             psk = secrets.token_hex(16)
@@ -534,6 +802,8 @@ if __name__ == "__main__":
 
             temp_parameters =  { 
                                  "device_sys_ip":device["system_ip"],
+                                 "pri_ipsec_id": pri_ipsec_id,
+                                 "pri_ipsec_ip": pri_ipsec_ip,
                                  "viptela_mx_primary_src_ip": source_ip,
                                  #"viptela_mx_primary_dst_ip": device['mx_dst_ip'],
                                  "viptela_mx_primary_dst_ip": pubs,
@@ -550,7 +820,10 @@ if __name__ == "__main__":
                 logger.info("\nTunnel parameters are " + str(ipsec_parameters))
 
         device_info = ipsec_tunnel.get_device_templateid(device_template_name)
-            
+
+        device_info["device_template_name"] = device_template_name
+        device_info["service_vpn_ipsec_route"] = service_vpn_ipsec_route
+
         feature_templateids = ipsec_tunnel.get_feature_templates(device_info["device_template_id"])
 
         ipsec_templateid = ipsec_tunnel.create_ipsec_templates(device_info)
